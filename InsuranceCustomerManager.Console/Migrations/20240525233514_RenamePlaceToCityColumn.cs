c9a48655c6e79fbf0b0b6894de66eff60411c9e4
LorisAccordino <loris.accordino@gmail.com>
2024-05-26 01:39:26 +0200

    DB column rename migration

---
+++ InsuranceCustomerManager.Console/Migrations/20240525233514_RenamePlaceToCityColumn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsuranceCustomerManager.Console.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamePlaceToCityColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlaceOfBirth",
+                table: "Customers",
+                newName: "CityOfBirth");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CityOfBirth",
+                table: "Customers",
+                newName: "PlaceOfBirth");
+        }
+    }
+}
